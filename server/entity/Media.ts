7fca90d217462a015f5faa895a55128261c251b1
jabloink <github@jabloink.com>
2026-03-19 09:12:05 -0400

    fix(readarr): use Id instead of Slug for externalUrl

--- server/entity/Media.ts
+++ server/entity/Media.ts
@@ -332,7 +332,7 @@ class Media {
     }
 
     if (this.mediaType === MediaType.BOOK) {
-      if (this.serviceId !== null && this.externalServiceSlug !== null) {
+      if (this.serviceId !== null && this.externalServiceId !== null) {
         const settings = getSettings();
         const server = settings.readarr.find(
           (readarr) => readarr.id === this.serviceId
@@ -340,12 +340,12 @@ class Media {
 
         if (server) {
           this.serviceUrl = server.externalUrl
-            ? `${server.externalUrl}/book/${this.externalServiceSlug}`
-            : ReadarrAPI.buildUrl(server, `/book/${this.externalServiceSlug}`);
+            ? `${server.externalUrl}/book/${this.externalServiceId}`
+            : ReadarrAPI.buildUrl(server, `/book/${this.externalServiceId}`);
         }
       }
 
-      if (this.serviceId4k !== null && this.externalServiceSlug4k !== null) {
+      if (this.serviceId4k !== null && this.externalServiceId4k !== null) {
         const settings = getSettings();
         const server = settings.readarr.find(
           (readarr) => readarr.id === this.serviceId4k
@@ -353,11 +353,8 @@ class Media {
 
         if (server) {
           this.serviceUrl4k = server.externalUrl
-            ? `${server.externalUrl}/book/${this.externalServiceSlug4k}`
-            : ReadarrAPI.buildUrl(
-                server,
-                `/book/${this.externalServiceSlug4k}`
-              );
+            ? `${server.externalUrl}/book/${this.externalServiceId4k}`
+            : ReadarrAPI.buildUrl(server, `/book/${this.externalServiceId4k}`);
         }
       }
     }
